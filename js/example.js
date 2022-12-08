4341f9c71ad2dd6343d51b297b278baf0e8bc006
s1uth <handtaein@gmail.com>
2022-12-08 16:18:02 +0900

    homeWork add

--- js/example.js
+++ js/example.js
@@ -1,16 +1,50 @@
-const nameH1Element = document.querySelector('h1.inline');
-const connectNameElement = document.querySelector('span.red');
-const inputModalElement = document.querySelector('dialog#inputModal');
+const nameH1Element = document.querySelector('h1.inline')
+const connectNameElement = document.querySelector('span.red')
+const inputModalElement = document.querySelector('dialog#inputModal')
 const modalSubmitBtn = document.querySelector('button.modalSubmit')
+const studentNumberElement = document.querySelector('span.studentNum')
+const studentEmailElement = document.querySelector('span.studentEmail')
 
-const localName = localStorage.getItem("userName");
+const localName = localStorage.getItem("userName")
+const localNum = localStorage.getItem("userNumber")
+const localEmail = localStorage.getItem("userEmail")
+
+const checkNumber =(Num)=>{
+    if(isNaN(Num)){
+        return false
+    }
+    else{
+        return true
+    }
+}
+const CheckEmail = (eMail) => {                                                
+     var reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+     if(!reg_email.test(eMail)) {                            
+       return false      
+     }       
+    else{
+        return true
+    }                     
+}                            
 
 const setName = (name) => {
     nameH1Element.textContent = name
     connectNameElement.textContent = name
 }
+const setStudentNum = (stNum) =>{
+    studentNumberElement.textContent = stNum
+}
+const setStudentEmail = (stEmail) =>{
+    studentEmailElement.textContent = stEmail
+}
 
-if(localName) setName(localName)
+if(localName && localNum && localEmail){
+    setName(localName)
+    setStudentEmail(localEmail)
+    setStudentNum(localNum)
+} 
+
 
 nameH1Element.onclick = () => {
     inputModalElement.showModal()
@@ -24,15 +58,21 @@ nameH1Element.onclick = () => {
 }
 
 modalSubmitBtn.onclick=()=>{
+    
     const modalFormElement = document.querySelector('.modalForm')
     const formData = new FormData(modalFormElement)
 
     for(const[key, value] of formData){
         localStorage.setItem(key, value)
     }
+    if(checkNumber(localStorage.getItem("userNumber"))&&
+        CheckEmail(localStorage.getItem("userEmail"))){
 
-    if(key === 'userName') setName(value)
     inputModalElement.close()
+    }
+    else{
+        alert("잘못된 입력 입니다.")
+    }
 }
 
 inputModalElement.onclick = (event) => {
