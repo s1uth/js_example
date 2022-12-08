fb666a0aba61332c8ad38bb0eb33ba6c8155904d
s1uth <handtaein@gmail.com>
2022-12-08 14:38:00 +0900

    localStorage ex add

--- js/example.js
+++ js/example.js
@@ -1,5 +1,23 @@
 const nameH1Element = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
 
-nameH1Element.onClick = () =>{
-    alert('클릭되었습니다.');
-};
+const localName = localStorage.getItem("userName");
+
+if(localName) {
+    nameH1Element.textContent = localName;
+    connectNameElement.textContent = localName;
+}
+
+nameH1Element.onclick = () => {
+    const inputName = prompt("이름을 입력해 주세요");
+
+    if(inputName){
+        localStorage.setItem("userName", inputName)
+
+        nameH1Element.textContent = inputName;
+        connectNameElement.textContent = inputName;
+    }else{
+        alert("입력되지 않았습니다.");
+    }
+ 
+}
