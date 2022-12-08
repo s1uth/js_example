1f06e5de8f88fc1500756ad645d6de5ae74f70e5
s1uth <handtaein@gmail.com>
2022-12-08 15:32:02 +0900

    formdata add

--- js/example.js
+++ js/example.js
@@ -1,27 +1,38 @@
 const nameH1Element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
 const inputModalElement = document.querySelector('dialog#inputModal');
+const modalSubmitBtn = document.querySelector('button.modalSubmit')
 
 const localName = localStorage.getItem("userName");
 
 const setName = (name) => {
-    nameH1Element.textContent = name;
-    connectNameElement.textContent = name;
+    nameH1Element.textContent = name
+    connectNameElement.textContent = name
 }
 
-if(localName) setName(localName);
+if(localName) setName(localName)
 
 nameH1Element.onclick = () => {
-    inputModalElement.showModal();
-
+    inputModalElement.showModal()
     //const inputName = prompt("이름을 입력해 주세요");
-
     // if(inputName){
     //     localStorage.setItem("userName", inputName)
     //     setName(inputName);
     // }else{
     //     alert("입력되지 않았습니다.");
     // }
+}
+
+modalSubmitBtn.onclick=()=>{
+    const modalFormElement = document.querySelector('.modalForm')
+    const formData = new FormData(modalFormElement)
+
+    for(const[key, value] of formData){
+        localStorage.setItem(key, value)
+    }
+
+    if(key === 'userName') setName(value)
+    inputModalElement.close()
 }
 
 inputModalElement.onclick = (event) => {
